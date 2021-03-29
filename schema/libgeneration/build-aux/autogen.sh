2aeb1c723d7d73507c8592f5e17d61ad2897046a
Yury Rusinov <yrusinov@gmail.com>
2021-03-29 23:54:35 +0300

    Old directory was removed.
    Signed-off-by: Yury Rusinov <yrusinov@gmail.com>

--- schema/libgeneration/build-aux/autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -xe
-
-libtoolize                      $LT_EXTRA
-aclocal                         $ACLOCAL_EXTRA
-autoheader                      $AH_EXTRA
-automake     --add-missing      $AM_EXTRA
-autoconf                        $AC_EXTRA
-
