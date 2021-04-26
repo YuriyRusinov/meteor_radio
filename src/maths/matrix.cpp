54ed1d4b9c1d32b4c8c27ed61fbada2fbe340606
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2021-04-26 18:51:20 +0300

    Matrix header was declared.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

---
+++ src/maths/matrix.cpp
@@ -0,0 +1,15 @@
+/*
+ * @brief Класс матриц, используемых в расчетах
+ * matrix.cpp
+ *
+ * (C) НИИ "Рубин"
+ * @author
+ *  Ю.Л.Русинов
+ */
+
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_permutation.h>
+#include "matrix.h"
+
+
---
+++ src/maths/matrix.h
@@ -0,0 +1,36 @@
+/*
+ * @brief Класс матриц, используемых в расчетах
+ * matrix.h
+ *
+ * (C) НИИ "Рубин"
+ * @author
+ *  Ю.Л.Русинов
+ */
+
+#pragma	once
+
+class gsl_vector;
+class gsl_matrix;
+class gsl_permutation;
+
+class Matrix {
+public:
+    Matrix( const double* a_data=nullptr, size_t nrows=0, size_t ncols=0 );
+    Matrix( double value, int nrows=0, int ncols=0 );
+    Matrix( const Matrix& M );
+    ~Matrix();
+
+    const double& operator() ( const size_t i, const size_t j ) const;
+    double& operator() ( const size_t i, const size_t j );
+
+    Matrix& operator=( const Matrix& M );
+
+    Matrix& operator+=( const Matrix& M );
+    Matrix& operator-=( const Matrix& M );
+    Matrix& operator*=( const Matrix& M );
+    Matrix& operator*=( double alamb );
+private:
+    gsl_matrix* _mData;
+    size_t _nRows;
+    size_t _nColumns;
+};
