69cacaf8729f44a58246644daa7d6289fddbee67
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2021-05-21 17:18:43 +0300

    Some functions for db.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- schema/messages/insertmessage.sql
+++ schema/messages/insertmessage.sql
@@ -13,6 +13,9 @@ begin
     if( idSent is null ) then
         raise warning 'Cannot find station with address %', address;
         return -1;
+    elsif( idSent = id_from ) then
+        raise warning 'Cannot send message to the same station';
+        return -1;
     end if;
     select into idMessage getnextseq('tbl_meteor_messages', 'id');
     insert into tbl_meteor_messages( id, message_content, id_meteor_station_from, id_meteor_station_to ) values ( idMessage, content, id_from, idSent );
